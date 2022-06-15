dfe11079945cd9b5827a0e38011b55abd821687f
shreysheth <shreysheth6487@gmail.com>
2022-06-15 22:19:57 +0530

    Added Home Section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "./components/Navbar.css";
+import Home from "./components/Home";
+import "./components/Home.css";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<section id="home">
+				<Home />
+			</section>
 		</div>
 	);
 }
